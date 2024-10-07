513d8922be1e09af5fa109743c2c1de26e501f8d
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2024-10-07 19:49:33 +0200

    presentation monorepo

---
+++ apps/presentation/app/(marketing)/page.tsx
@@ -0,0 +1,25 @@
+import ClientSection from "@/components/landing/client-section";
+import CallToActionSection from "@/components/landing/cta-section";
+import HeroSection from "@/components/landing/hero-section";
+import PricingSection from "@/components/landing/pricing-section";
+import Particles from "@/components/magicui/particles";
+import { SphereMask } from "@/components/magicui/sphere-mask";
+import PoweringIntelliq from "@/components/landing/powering-intelliq";
+export default async function Page() {
+  return (
+    <>
+      <HeroSection />
+      <PoweringIntelliq />
+      <SphereMask />
+      {/* <CallToActionSection /> */}
+      <Particles
+        className="absolute inset-0 -z-10"
+        quantity={50}
+        ease={70}
+        size={0.05}
+        staticity={40}
+        color={"#ffffff"}
+      />
+    </>
+  );
+}
