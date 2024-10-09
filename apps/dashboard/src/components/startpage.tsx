8b0ac179cb61be83000ecac1a24d0b591a0b964f
RickyRAV <rickyraveanu02@gmail.com>
2024-10-10 01:16:50 +0200

    refactor: restructure for everything

---
+++ apps/dashboard/src/components/startpage.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import Hero from "./hero";
+import AnimatedGridPattern from "./ui/animated-grid-pattern";
+import { Testimonials } from "./testimonials";
+
+export default function StartPage() {
+  return (
+    <div className="relative">
+      <div className="px-4">
+        <Hero />
+      </div>
+      <AnimatedGridPattern
+        numSquares={30}
+        maxOpacity={0.6}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(750px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-40%] h-[90%] skew-y-12 -z-10"
+        )}
+      />
+      <div className="mt-[300px]">
+        <Testimonials />
+      </div>
+    </div>
+  );
+}
