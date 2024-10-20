5be393ba5ff03b22861a23f5c13afce8642ab5f3
RickyRAV <rickyraveanu02@gmail.com>
2024-10-20 17:14:31 +0200

    IDA-410 pls kill me

--- apps/web/src/components/cube.tsx
+++ apps/web/src/components/cube.tsx
@@ -1,16 +1,19 @@
 import Spline from "@splinetool/react-spline/next";
+import React, { Suspense } from "react";
 
 export async function Cube() {
   return (
     <div className="animate-webgl-scale-in-fade">
-      <Spline
-        scene="https://prod.spline.design/7f3iuI3rFscphrdO/scene.splinecode"
-        style={{
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline
+          scene="https://prod.spline.design/7f3iuI3rFscphrdO/scene.splinecode"
+          style={{
           width: "auto",
           height: "auto",
-          background: "transparent",
-        }}
-      />
+            background: "transparent",
+          }}
+        />
+      </Suspense>
     </div>
   );
 }
