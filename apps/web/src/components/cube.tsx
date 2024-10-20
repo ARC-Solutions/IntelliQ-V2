0d5fcb39c3a45a9c058970d37fe71d028159295a
RickyRAV <rickyraveanu02@gmail.com>
2024-10-20 20:21:58 +0200

    some bs model found somewhere

--- apps/web/src/components/cube.tsx
+++ apps/web/src/components/cube.tsx
@@ -6,7 +6,7 @@ export async function Cube() {
     <div className="animate-webgl-scale-in-fade">
       <Suspense fallback={<div>Loading...</div>}>
         <Spline
-          scene="https://prod.spline.design/6en5-sGU9Wp4QI5m/scene.splinecode"
+          scene="https://prod.spline.design/pvM5sSiYV2ivWraz/scene.splinecode"
           style={{
             width: "auto",
             height: "auto",
