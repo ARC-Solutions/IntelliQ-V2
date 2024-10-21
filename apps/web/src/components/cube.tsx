898d75a444b297b8c12a831d1680f0456178ad62
RickyRAV <rickyraveanu02@gmail.com>
2024-10-21 02:46:49 +0200

    ida-410 might be dumb but it works

--- apps/web/src/components/cube.tsx
+++ apps/web/src/components/cube.tsx
@@ -1,19 +1,38 @@
+"use client";
+
 import Spline from "@splinetool/react-spline/next";
 import React, { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
 export async function Cube() {
+  const searchParams = useSearchParams();
+  const viewport = searchParams.get("viewport");
+
   return (
     <div className="animate-webgl-scale-in-fade">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Spline
-          scene="https://prod.spline.design/pvM5sSiYV2ivWraz/scene.splinecode"
-          style={{
-            width: "auto",
-            height: "auto",
-            background: "transparent",
-          }}
-        />
-      </Suspense>
+      {viewport === "mobile" ? (
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-auto max-w-[300px]"
+        >
+          <source src="/cubic.webm" type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      ) : (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Spline
+            scene="https://prod.spline.design/H4VB9VxDKY26loFd/scene.splinecode"
+            style={{
+              width: "auto",
+              height: "auto",
+              background: "transparent",
+            }}
+          />
+        </Suspense>
+      )}
     </div>
   );
 }
---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse, userAgent } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl;
+  const { device } = userAgent(request);
+  const viewport = device.type === "mobile" ? "mobile" : "desktop";
+  url.searchParams.set("viewport", viewport);
+  console.log(viewport);
+  return NextResponse.rewrite(url);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
