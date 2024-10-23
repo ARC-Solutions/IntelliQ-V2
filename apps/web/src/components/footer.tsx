7cad8398d6402c7a11f4046b4b95539fba8ab47c
RickyRAV <rickyraveanu02@gmail.com>
2024-10-23 20:14:29 +0200

    IDA-419

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
-import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 import { SocialLinks } from "./social-links";
 import { Wordmark } from "./wordmark";
 
@@ -32,13 +34,19 @@ const navigation = [
 ];
 
 export function Footer() {
+  const { resolvedTheme } = useTheme();
   return (
     <>
       <footer className="border-t-[1px] border-border px-4 md:px-6 pt-10 md:pt-16 overflow-hidden md:max-h-[820px]">
         <div className="container mx-auto w-full">
           <div className="flex justify-between items-center border-border border-b-[1px] pb-10 md:pb-16 mb-12">
             <Link href="/" className="scale-50 -ml-[52px] md:ml-0 md:scale-100">
-              <Image src="/logo.svg" alt="Midday" width={100} height={100} />
+              <Image
+                src={resolvedTheme === "dark" ? "/logo-dark.svg" : "/logo.svg"}
+                alt="IntelliQ"
+                width={100}
+                height={100}
+              />
             </Link>
             <span className="font-normal md:text-2xl text-right">
               Start your AI quiz today!
--- apps/web/src/components/testimonials.tsx
+++ apps/web/src/components/testimonials.tsx
@@ -94,8 +94,7 @@ const ReviewCard = ({
 
 export function Testimonials() {
   return (
-    // <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg md:shadow-xl">
-    <div className="relative h-[400px] w-full flex-col items-center justify-center overflow-hidden rounded-lg md:shadow-xl">
+    <div className="relative h-[400px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
       <Marquee pauseOnHover className="[--duration:20s]">
         {firstRow.map((review) => (
           <ReviewCard key={review.username} {...review} />
@@ -106,8 +105,8 @@ export function Testimonials() {
           <ReviewCard key={review.username} {...review} />
         ))}
       </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-background to-transparent"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-background to-transparent"></div>
     </div>
   );
 }
