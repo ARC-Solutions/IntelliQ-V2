07f766bbf63042cb197435d2f81e9d1cd460445a
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2024-11-05 23:52:14 +0100

    Update auth.ts

--- apps/dashboard/src/lib/validations/auth.ts
+++ apps/dashboard/src/lib/validations/auth.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// im trying to see something for aws amplify
 export const loginSchema = z.object({
   email: z.string().email("Invalid email format"),
   password: z.string().min(6, "Password must be at least 6 characters"),
