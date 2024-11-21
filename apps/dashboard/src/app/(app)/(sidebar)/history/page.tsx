369a2c85c7fe5aa64f3460cbf55cf828c5b3feb7
Nippon Lama <102690989+Noppin@users.noreply.github.com>
2024-11-21 19:41:40 +0100

    IDA-427

--- apps/dashboard/src/app/(app)/(sidebar)/history/page.tsx
+++ apps/dashboard/src/app/(app)/(sidebar)/history/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { createClient } from '@/lib/supabase/supabase-server-side';
+import { fetchAllQuizzes } from '@/lib/fetch-all-quizzes';
+import HistoryPage from '@/components/history-page/history-page';
+import { CalendarIcon, ClockIcon } from 'lucide-react';
+const History = async () => {
+  const supabase = createClient();
+  const { session } = (await supabase.auth.getSession()).data;
+  const accessToken = session?.access_token as string;
+  const data = await fetchAllQuizzes(accessToken, 0);
 
-const History = () => {
-  return <div>History</div>;
+  return <HistoryPage historyQuizzes={data} />;
 };
 
 export default History;
