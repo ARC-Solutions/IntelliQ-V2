006f9f200d39fd6602a82d095f041bb211e30c00
Nikola <118454864+Nikola-Petro@users.noreply.github.com>
2024-11-22 10:01:12 +0100

    IDA-447 Error

--- apps/presentation/src/components/landing/visual-section.tsx
+++ apps/presentation/src/components/landing/visual-section.tsx
@@ -4,39 +4,41 @@ import { useEffect, useRef } from 'react';
 
 export default function VisualEnhance() {
 
-  const canvasRef = useRef();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     let phi = 0;
 
-    const globe = createGlobe(canvasRef.current, {
-      devicePixelRatio: 2,
-      width: 650 * 2,
-      height: 650 * 2,
-      phi: 0,
-      theta: 0.2,
-      dark: 1,
-      diffuse: 1.2,
-      mapSamples: 16000,
-      mapBrightness: 6,
-      baseColor: [0.784, 0.714, 1],
-      markerColor: [0.784, 0.714, 1],
-      glowColor: [0.784, 0.714, 1],
-      markers: [
-        // longitude latitude
-        { location: [50.1109, 8.6821], size: 0.1 },
-      ],
-      onRender: (state) => {
-        // Called on every animation frame.
-        // `state` will be an empty object, return updated params.
-        state.phi = phi;
-        phi += 0.01;
-      },
-    });
+    if (canvasRef.current) {
+      const globe = createGlobe(canvasRef.current, {
+        devicePixelRatio: 2,
+        width: 650 * 2,
+        height: 650 * 2,
+        phi: 0,
+        theta: 0.2,
+        dark: 1,
+        diffuse: 1.2,
+        mapSamples: 16000,
+        mapBrightness: 6,
+        baseColor: [0.784, 0.714, 1],
+        markerColor: [0.784, 0.714, 1],
+        glowColor: [0.784, 0.714, 1],
+        markers: [
+          // longitude latitude
+          { location: [50.1109, 8.6821], size: 0.1 },
+        ],
+        onRender: (state) => {
+          // Called on every animation frame.
+          // `state` will be an empty object, return updated params.
+          state.phi = phi;
+          phi += 0.01;
+        },
+      });
 
-    return () => {
-      globe.destroy();
-    };
+      return () => {
+        globe.destroy();
+      };
+    }
   }, []);
 
   return (
