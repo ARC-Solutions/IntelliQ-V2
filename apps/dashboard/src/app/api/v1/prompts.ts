1a20fa5f0c32241e63626684933c2cc758e14bb9
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2024-11-27 22:17:12 +0100

    IDA-451 [skip-cd] smaller prompt

--- apps/dashboard/src/app/api/v1/prompts.ts
+++ apps/dashboard/src/app/api/v1/prompts.ts
@@ -13,26 +13,6 @@ ${
     : ""
 }
 
-The JSON object should be structured as follows:
-
-  {
-    "quizTitle": "Contextual and Unique Quiz Title Here",
-    "questions": [
-      {
-        "questionTitle": "Unique and Contextual Question Title Here",
-        "text": "The actual question here?",
-        "options": [
-          "a) Option 1",
-          "b) Option 2",
-          "c) Option 3",
-          "d) Option 4"
-        ],
-        "correctAnswer": "a) Option 1"
-      }
-      // More questions here...
-    ]
-  }
-
 Once the quizTitle is set, it should not change. Each question should have a unique questionTitle. 
 The questions should have exactly four options labeled a), b), c), and d). 
 The Contextual questionTitle is not allowed to contain 'Question Number' or 'Interest Question Number', 
--- apps/dashboard/src/app/api/v1/quizzes/generate/route.ts
+++ apps/dashboard/src/app/api/v1/quizzes/generate/route.ts
@@ -3,7 +3,7 @@ import { openai } from "@ai-sdk/openai";
 import { generateObject } from "ai";
 import { quizGenerationRequestSchema, quizSchema } from "@/app/api/v1/schemas";
 import { generateQuizPrompt } from "@/app/api/v1/prompts";
-import { createClient } from "../../../../../lib/supabase/supabase-server-side";
+import { createClient } from "@/lib/supabase/supabase-server-side";
 
 export const GET = async (request: NextRequest) => {
   try {
@@ -11,6 +11,7 @@ export const GET = async (request: NextRequest) => {
     const {
       data: { user },
     } = await supabase.auth.getUser();
+
     const { searchParams } = request.nextUrl;
     const result = quizGenerationRequestSchema.safeParse({
       quizTopic: searchParams.get("quizTopic"),
