dd31aff9ee51353f2c5b1e9d5728915bdf6716ee
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2024-12-03 20:50:12 +0100

    vercel ?

--- apps/dashboard/src/app/api/v1/route.ts
+++ apps/dashboard/src/app/api/v1/route.ts
@@ -1,9 +1,5 @@
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
-
 export const GET = async () => {
   return NextResponse.json({ message: "Hello, world!" });
 };
