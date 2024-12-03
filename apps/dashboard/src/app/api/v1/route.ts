9f043825be90ffacd6bb08636964d968711e6688
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2024-12-03 17:32:59 +0100

    bruh

--- apps/dashboard/src/app/api/v1/route.ts
+++ apps/dashboard/src/app/api/v1/route.ts
@@ -1,13 +1,5 @@
-import { db } from "@/db/index";
 import { NextResponse } from "next/server";
-import { users } from "@/../drizzle/schema";
 
 export const GET = async () => {
-  const result = await db
-    .select({
-      email: users.email,
-    })
-    .from(users);
-
-  return NextResponse.json(result);
+  return NextResponse.json({ message: "Hello, world!" });
 };
