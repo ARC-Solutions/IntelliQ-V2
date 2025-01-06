3be5782d41edde359592cda874dd0fa617adafe1
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-06 03:08:40 +0100

    cloudflare

--- apps/dashboard/src/app/(app)/(sidebar)/page.tsx
+++ apps/dashboard/src/app/(app)/(sidebar)/page.tsx
@@ -1,6 +1,8 @@
 import { DashboardFeatures } from '@/components/dashboard/dashboard-features';
 import React from 'react';
 
+export const runtime = "edge";
+
 const Dashboard = async () => {
   return (
     <div className='relative flex flex-col h-screen w-screen items-center justify-center rounded-lg bg-background md:shadow-xl'>
