b98dc6538f0535b26259ab0386b2a1c7a3b809b9
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-15 00:18:46 +0100

    IDA-508

--- apps/dashboard/src/lib/supabase/supabase-server-side.ts
+++ apps/dashboard/src/lib/supabase/supabase-server-side.ts
@@ -3,6 +3,12 @@ import { cookies } from 'next/headers';
 
 export function createClient() {
   const cookieStore = cookies();
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
+  }
+  if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
+  }
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
