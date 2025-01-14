943fe5f204a0869f51ea6ea5182d23eb46ce5582
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-15 00:18:46 +0100

    IDA-508

--- apps/dashboard/src/lib/supabase/supabase-client-side.ts
+++ apps/dashboard/src/lib/supabase/supabase-client-side.ts
@@ -1,8 +1,15 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
+  }
+  if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
+  }
+
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 }
