934a69e83a84476e9b6b3e5c11241a7eece3c463
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-15 10:04:35 +0100

    BACK TO CF PAGES

--- apps/dashboard/src/lib/supabase/supabase-server-side.ts
+++ apps/dashboard/src/lib/supabase/supabase-server-side.ts
@@ -2,15 +2,7 @@ import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
 export function createClient() {
-  const cookieStore = cookies();
-  
-  if (!process.env.SUPABASE_URL) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_URL or ENV is not defined");
-  }
-  if (!process.env.SUPABASE_KEY) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY or ENV is not defined");
-  }
-  
+  const cookieStore = cookies();  
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
