f889fa6b5324e56a56389339684c33c5ace2f42b
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-15 10:04:35 +0100

    BACK TO CF PAGES

--- apps/dashboard/src/lib/supabase/supabase-client-side.ts
+++ apps/dashboard/src/lib/supabase/supabase-client-side.ts
@@ -1,13 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  if (!process.env.SUPABASE_URL) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_URL or SUPABASE_URL is not defined");
-  }
-  if (!process.env.SUPABASE_KEY) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY or SUPABASE_KEY is not defined");
-  }
-
   return createBrowserClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_KEY!
