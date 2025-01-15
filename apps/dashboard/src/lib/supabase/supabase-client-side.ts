2bdf153eb80b3be225854d9afb4b0e3bdb5ce320
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-15 01:20:59 +0100

    hyperdrive type

--- apps/dashboard/src/lib/supabase/supabase-client-side.ts
+++ apps/dashboard/src/lib/supabase/supabase-client-side.ts
@@ -1,15 +1,21 @@
 import { createBrowserClient } from "@supabase/ssr";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 
-export function createClient() {
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
+export async function createClient() {
+  const supabase_anon = (await getCloudflareContext()).env
+    .NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabase_url = (await getCloudflareContext()).env
+    .NEXT_PUBLIC_SUPABASE_URL;
+  
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !supabase_url) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_URL or URL is not defined");
   }
-  if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
+  if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || !supabase_anon) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY or ANON is not defined");
   }
 
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL || supabase_url,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || supabase_anon
   );
 }
--- apps/dashboard/src/lib/supabase/supabase-server-side.ts
+++ apps/dashboard/src/lib/supabase/supabase-server-side.ts
@@ -1,27 +1,39 @@
+import { getCloudflareContext } from '@opennextjs/cloudflare';
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export function createClient() {
-  const cookieStore = cookies();
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
-  }
-  if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
-  }
+export interface Env {
+  // If you set another name in wrangler.toml as the value for 'binding',
+  // replace "HYPERDRIVE" with the variable name you defined.
+  HYPERDRIVE: Hyperdrive;
+}
 
+export async function createClient() {
+  const cookieStore = cookies();
+  const supabase_anon = (await getCloudflareContext()).env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabase_url = (await getCloudflareContext()).env.NEXT_PUBLIC_SUPABASE_URL;
+  
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !supabase_url) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_URL or ENV is not defined");
+  }
+  if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || !supabase_anon) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY or ENV is not defined");
+  }
+  
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL! || supabase_url,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || supabase_anon,
     {
       cookies: {
         getAll() {
           return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
         },
       },
-    },
+    }
   );
 }
