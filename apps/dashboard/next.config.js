7518efe1e113edb1363f6817e7e4f2c2b71d51b7
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-16 18:12:27 +0100

    edge hihi haha kill me pls

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,7 +1,17 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      stream: false,
+      crypto: false,
+      perf_hooks: false
+    };
+    return config;
+  },
+};
 
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
