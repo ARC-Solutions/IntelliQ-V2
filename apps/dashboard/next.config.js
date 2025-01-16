7d8a4fb6c3f1282499ab30882042cc9714437afa
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-16 18:14:40 +0100

    rookie

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,20 +1,20 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
+      path: false,
       stream: false,
-      crypto: false,
-      perf_hooks: false
+      constants: false,
     };
     return config;
   },
 };
 
 if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
+  setupDevPlatform();
 }
 
-export default nextConfig;
+module.exports = nextConfig;
