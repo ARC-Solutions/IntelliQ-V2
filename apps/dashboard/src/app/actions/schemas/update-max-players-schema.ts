00033054d2f502ec955b1a4637332e0ecb6bce20
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-17 00:04:34 +0100

    jid

--- apps/dashboard/src/app/actions/schemas/update-max-players-schema.ts
+++ apps/dashboard/src/app/actions/schemas/update-max-players-schema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import {roomSchema} from "@/app/api/v1/schemas"
+import {roomSchema} from "@/app/actions/schemas/old-v1-api-schema"
 
 export const updateMaxPlayersSchema = z.object({
     maxPlayers: z.number().min(2).max(10),
