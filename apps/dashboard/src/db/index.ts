9668e46001831a7834d837dffb74a4c2a082b701
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-16 02:33:31 +0100

    im going to sleep after this

--- apps/dashboard/src/db/index.ts
+++ apps/dashboard/src/db/index.ts
@@ -1,10 +1,12 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon, neonConfig } from "@neondatabase/serverless";
 import { getRequestContext } from "@cloudflare/next-on-pages";
+
+neonConfig.fetchConnectionCache = true;
 
 export const getDb = () => {
   const { env } = getRequestContext();
-  const queryClient = postgres(env.HYPERDRIVE.connectionString);
-  const db = drizzle({ client: queryClient });
+  const sql = neon(env.HYPERDRIVE.connectionString);
+  const db = drizzle(sql);
   return db;
 };
