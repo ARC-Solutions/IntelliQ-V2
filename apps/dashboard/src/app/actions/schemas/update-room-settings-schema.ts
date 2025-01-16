e0d630ca8fd714b5e335c527465f09e42e1f2112
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-17 00:04:34 +0100

    jid

--- apps/dashboard/src/app/actions/schemas/update-room-settings-schema.ts
+++ apps/dashboard/src/app/actions/schemas/update-room-settings-schema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { roomSchema } from "@/app/api/v1/schemas";
+import { roomSchema } from "@/app/actions/schemas/old-v1-api-schema";
 
 export const updateRoomSettingsSchema = z.object({
   roomCode: roomSchema.shape.roomCode,
