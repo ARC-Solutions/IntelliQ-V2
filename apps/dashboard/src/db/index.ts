7b8657209ab0b95402339f85cba6bd84be18bdb1
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-16 03:08:24 +0100

    LALLALALALLAL

--- apps/dashboard/src/db/index.ts
+++ apps/dashboard/src/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-serverless";
 import { getRequestContext } from "@cloudflare/next-on-pages";
+import ws from "ws";
 
 export const getDb = () => {
   const { env } = getRequestContext();
-  const db = drizzle(env.HYPERDRIVE.connectionString);
+  const db = drizzle({ connection: env.HYPERDRIVE.connectionString, ws: ws });
   return db;
 };
