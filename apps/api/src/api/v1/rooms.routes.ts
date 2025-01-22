8f148a2dc2d074e386f6b6e311453876a5c4ba34
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-22 20:12:11 +0100

    response schemas

--- apps/api/src/api/v1/rooms.routes.ts
+++ apps/api/src/api/v1/rooms.routes.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
-import { roomSchema } from "./schemas/room.schemas";
+import { roomDetailsResponseSchema, roomResponseSchema, roomSchema } from "./schemas/room.schemas";
 import { createDb } from "../../db/index";
 import { describeRoute } from "hono-openapi";
-import { validator as zValidator } from "hono-openapi/zod";
+import { resolver, validator as zValidator } from "hono-openapi/zod";
 import { eq } from "drizzle-orm";
 import { rooms } from "../../../drizzle/schema";
+import { z } from "zod";
 
 const app = new Hono<{ Bindings: CloudflareEnv }>()
   .get(
@@ -13,24 +14,57 @@ const app = new Hono<{ Bindings: CloudflareEnv }>()
       tags: ["Rooms"],
       summary: "Get room details",
       description: "Get room details by room code",
+      validateResponse: true,
+      responses: {
+        200: {
+          description: "Room details",
+          content: {
+            "application/json": {
+              schema: resolver(roomResponseSchema),
+            },
+          },
+        },
+        400: {
+          description: "Bad Request",
+          content: {
+            "application/json": {
+              schema: z.object({
+                error: z.string(),
+              }),
+            },
+          },
+        },
+      },
     }),
     zValidator("param", roomSchema),
     async (c) => {
-      const {roomCode} = c.req.valid("param");
+      const { roomCode } = c.req.valid("param");
 
       const db = await createDb(c);
       const { maxPlayers } = (await db.query.rooms.findFirst({
         where: (rooms) => eq(rooms.code, roomCode),
       }))!;
 
-      return c.json({max_players: maxPlayers});
+      return c.json({ max_players: maxPlayers });
     }
   )
-  .get("/:roomCode/details",
+  .get(
+    "/:roomCode/details",
     describeRoute({
       tags: ["Rooms"],
       summary: "Get room details",
       description: "Get room details by room code",
+      validateResponse: true,
+      responses: {
+        200: {
+          description: "Room details",
+          content: {
+            "application/json": {
+              schema: resolver(roomDetailsResponseSchema),
+            },
+          },
+        },
+      },
     }),
     zValidator("param", roomSchema),
     async (c) => {
@@ -43,6 +77,6 @@ const app = new Hono<{ Bindings: CloudflareEnv }>()
 
       return c.json(room);
     }
-);
+  );
 
 export default app;
