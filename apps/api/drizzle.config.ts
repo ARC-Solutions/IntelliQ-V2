84f67cadef593ddeb3c2a5712edf4efb236ad876
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-25 19:54:14 +0100

    IDA-519

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -5,4 +5,7 @@ export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://postgres.yrdpjeewqjuwhjjpniju:SQD%25%5Ehrh%40%247a%25Ux%23e%263NTyp%40ZWFT%24D%26W52cT5AiZP%235L%40f5w%409%406T3%21%5E3PXC6P%24Z@aws-0-eu-central-1.pooler.supabase.com:5432/postgres",
+  },
 });
--- apps/dashboard/src/app/actions/schemas/update-max-players-schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-import {roomSchema} from "@/app/actions/schemas/old-v1-api-schema"
-
-export const updateMaxPlayersSchema = z.object({
-    maxPlayers: z.number().min(2).max(10),
-    roomCode: roomSchema.shape.roomCode,
-});
