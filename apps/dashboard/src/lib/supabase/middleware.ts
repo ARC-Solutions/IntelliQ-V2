4ae8f35cc96515b679d1c0f96b4c04910907397d
RickyRAV <rickyraveanu02@gmail.com>
2025-02-15 22:49:06 +0100

    new-404

--- apps/dashboard/src/lib/supabase/middleware.ts
+++ apps/dashboard/src/lib/supabase/middleware.ts
@@ -47,3 +47,9 @@ export async function updateSession(request: NextRequest) {
 
   return supabaseResponse;
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
