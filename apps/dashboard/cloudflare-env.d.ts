4f13d4e4fccbd671d3a291e6cfc1b9e74004b1e9
RickyRAV <rickyraveanu02@gmail.com>
2025-03-09 17:14:53 +0100

    Update cloudflare-env.d.ts

--- apps/dashboard/cloudflare-env.d.ts
+++ apps/dashboard/cloudflare-env.d.ts
@@ -2,14 +2,17 @@
 
 interface CloudflareEnv {
   IntelliQ_CACHE_VERSION: KVNamespace;
+  id: "e4ba004cd93a4e25853e135da77c9509";
+  ADMIN_TOKEN: string;
+  API_URL: string;
   AWS_ACCESS_KEY_ID: string;
   AWS_SECRET_ACCESS_KEY: string;
   DATABASE_URL: string;
   GPT_MODEL: string;
   OPENAI_API_KEY: string;
+  QSTASH_TOKEN: string;
   SUPABASE_ANON_KEY: string;
   SUPABASE_URL: string;
   UPSTASH_REDIS_REST_TOKEN: string;
   UPSTASH_REDIS_REST_URL: string;
-  HYPERDRIVE: Hyperdrive;
 }
