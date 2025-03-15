d883c263cc04257f04da94c021c9214924c894ea
Nippon Lama <102690989+Noppin@users.noreply.github.com>
2025-03-15 11:20:03 +0100

    responsive with scroll bar

--- apps/dashboard/src/components/nav-main.tsx
+++ apps/dashboard/src/components/nav-main.tsx
@@ -4,18 +4,14 @@ import { type LucideIcon } from 'lucide-react';
 
 import {
   SidebarGroup,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
 } from '@/components/ui/sidebar';
 import Link from 'next/link';
 import { useQuiz } from '@/contexts/quiz-context';
 import { useQuizCreation } from '@/contexts/quiz-creation-context';
-import { useQuizLogic } from '@/contexts/quiz-logic-context';
+
 import { useRouter } from 'next/navigation';
 
 export function NavMain({
